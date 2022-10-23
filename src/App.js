6a1b4bf59fc3ff80a92f7bdd03abdbce87fe9660
Yogitaba Gohil <yogitabagohil1212@gmail.com>
2022-10-23 21:56:07 +0300

    Add login, signup page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
 import Home from './pages/Home';
-
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div >
-      <Navbar />
+    <>
+      <AuthContextProvider>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          </Routes>
-    </div>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
